276fb531b067925073896b95a98667c358d4594e
tangyin025@126.com <tangyin025@126.com@7131523d-76fb-ee71-b069-c78a23ed5c85>
2012-02-20 15:14:03 +0000

    hardware version for myt3dlib/demo1_3

--- myd3dlib/src/myMesh.cpp
+++ myd3dlib/src/myMesh.cpp
@@ -3,6 +3,10 @@
 #include "myd3dlib.h"
 #include "rapidxml.hpp"
 
+#ifdef _DEBUG
+#define new new( _CLIENT_BLOCK, __FILE__, __LINE__ )
+#endif
+
 namespace my
 {
 	std::vector<D3DVERTEXELEMENT9> D3DVERTEXELEMENT9Set::BuildVertexElementList(void) const
@@ -496,6 +500,15 @@ namespace my
 		node_submesh = node_submeshes->first_node("submesh");
 		for(int face_i = 0; node_submesh != NULL; node_submesh = node_submesh->next_sibling(), submesh_i++)
 		{
+			DEFINE_XML_ATTRIBUTE_SIMPLE(material, submesh);
+			DEFINE_XML_ATTRIBUTE_BOOL_SIMPLE(use32bitindexes, submesh);
+			DEFINE_XML_ATTRIBUTE_BOOL_SIMPLE(usesharedvertices, submesh);
+			DEFINE_XML_ATTRIBUTE_SIMPLE(operationtype, submesh);
+			if(!usesharedvertices || 0 != _stricmp(attr_operationtype->value(), "triangle_list"))
+			{
+				THROW_CUSEXCEPTION("!usesharedvertices || !triangle_list");
+			}
+
 			DEFINE_XML_NODE_SIMPLE(faces, submesh);
 			DEFINE_XML_ATTRIBUTE_INT_SIMPLE(count, faces);
 
@@ -539,5 +552,106 @@ namespace my
 		mesh->OptimizeInplace(D3DXMESHOPT_ATTRSORT | D3DXMESHOPT_VERTEXCACHE, &rgdwAdjacency[0], NULL, NULL, NULL);
 
 		return mesh;
+	}
+
+	MeshPtr Mesh::CreateBox(
+		LPDIRECT3DDEVICE9 pd3dDevice,
+		FLOAT Width /*= 1.0f*/,
+		FLOAT Height /*= 1.0f*/,
+		FLOAT Depth /*= 1.0f*/,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreateBox(pd3dDevice, Width, Height, Depth, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
+	}
+
+	MeshPtr Mesh::CreateCylinder(
+		LPDIRECT3DDEVICE9 pd3dDevice,
+		FLOAT Radius1 /*= 1.0f*/,
+		FLOAT Radius2 /*= 1.0f*/,
+		FLOAT Length /*= 2.0f*/,
+		UINT Slices /*= 20*/,
+		UINT Stacks /*= 1*/,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreateCylinder(pd3dDevice, Radius1, Radius2, Length, Slices, Stacks, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
+	}
+
+	MeshPtr Mesh::CreatePolygon(
+		LPDIRECT3DDEVICE9 pDevice,
+		FLOAT Length /*= 1.0f*/,
+		UINT Sides /*= 5*/,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreatePolygon(pDevice, Length, Sides, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
+	}
+
+	MeshPtr Mesh::CreateSphere(
+		LPDIRECT3DDEVICE9 pDevice,
+		FLOAT Radius /*= 1.0f*/,
+		UINT Slices /*= 20*/,
+		UINT Stacks /*= 20*/,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreateSphere(pDevice, Radius, Slices, Stacks, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
+	}
+
+	MeshPtr Mesh::CreateTeapot(
+		LPDIRECT3DDEVICE9 pDevice,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreateTeapot(pDevice, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
+	}
+
+	MeshPtr Mesh::CreateTorus(
+		LPDIRECT3DDEVICE9 pDevice,
+		FLOAT InnerRadius /*= 0.5f*/,
+		FLOAT OuterRadius /*= 1.5f*/,
+		UINT Sides /*= 20*/,
+		UINT Rings /*= 20*/,
+		LPD3DXBUFFER * ppAdjacency /*= NULL*/)
+	{
+		LPD3DXMESH pMesh = NULL;
+		HRESULT hres = D3DXCreateTorus(pDevice, InnerRadius, OuterRadius, Sides, Rings, &pMesh, ppAdjacency);
+		if(FAILED(hres))
+		{
+			THROW_D3DEXCEPTION(hres);
+		}
+
+		return MeshPtr(new Mesh(pMesh));
 	}
 };
